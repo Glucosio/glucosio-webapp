d2e613491773a3020690a549cb8014ba5ed7338c
GabrieleTinelli <gabriele.tin@icloud.com>
2016-02-13 16:09:57 +0100

    Code organization totally changed using AngularJS

---
+++ app.js
@@ -0,0 +1,51 @@
+var app = angular.module('glucosio',[ ]);
+
+   app.directive('appTabs', function() {
+        return {
+            restrict: 'E',
+            templateUrl: "views/app-tabs.html",
+            <!-- actually not working -->
+            controller:function() {
+                this.tab = 1;
+
+                this.isSet = function(checkTab){
+                    return this.tab === checkTab;
+                };
+
+                this.setTab = function(activeTab) {
+                    this.tab = activeTab;
+                };
+            },
+            controllerAs: 'tabs'
+        };
+    });
+
+    app.directive('appBanner', function() {
+        return {
+            restrict: 'E',
+            templateUrl: 'views/app-banner.html',
+        }
+    });
+
+    app.directive('historyContent', function(){
+        return {
+            restrict: 'E',
+            templateUrl: 'views/history-content.html',
+        }
+    });
+
+    app.directive('assistantContent', function(){
+        return {
+            restrict: 'E',
+            templateUrl: 'views/assistant-content.html',
+        }
+    });
+
+    app.directive('footerContent', function(){
+      return {
+        restrict: 'E',
+        templateUrl: 'views/footer-content.html',
+      }
+    });
+
+    
